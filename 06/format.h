9eeda490a6cd5f8f9e22c599c3a6ab4361e0f68e
Denis <den1s.rogozhin@mail.ru>
2021-06-03 14:17:19 +0300

    second

--- 06/format.h
+++ 06/format.h
@@ -3,13 +3,14 @@
 #include <iostream>
 #include <cassert> 
 #include <sstream>
+#include <exception>
 
 
-class Error {
+class Error : public std::logic_error {
         std::string message;
         int position;
     public:
-	Error(const std::string & text, int pos) {
+	Error(const std::string & text, int pos): std::logic_error(text) {
 		message = text;
 		position = pos;
 	}
@@ -29,6 +30,17 @@ class Bad_bracket : public Error {
         Bad_bracket(const std::string& text, int pos) : Error(text, pos) {};
 };
 
+
+class Too_much_args : public Error {
+    public:
+        Too_much_args(const std::string& text, int pos) : Error(text, pos) {};
+};
+
+class Big_index : public Error {
+    public:
+        Big_index(const std::string& text, int pos) : Error(text, pos) {};
+};
+
 template <class T>
 std::stringstream get_arg(int ind, T first) {
     std::stringstream result;
@@ -36,6 +48,11 @@ std::stringstream get_arg(int ind, T first) {
     return result;
 }
 
+std::stringstream get_arg(int ind) {
+    std::stringstream result;
+    result << "";
+    return result;
+}
 
 template <class T, class ...Args>
 std::stringstream get_arg(int ind, T first, Args ...args) {
@@ -54,7 +71,9 @@ template <class ...Args>
 std::string format(const std::string & text, Args ... args) {
     std::string result;
     std::string::const_iterator p = text.begin();
+    int max_ind = -1;
     int num_of_arg = sizeof...(Args);
+    int max_num = 2147483647;
     while ( p != text.end()) {
 	if (*p != '{') {
 	    if ( *p == '}')
@@ -71,11 +90,16 @@ std::string format(const std::string & text, Args ... args) {
 		throw Bad_index("after { expected digit", p - text.begin() + 1) ;
 	    int ind = 0;
 	    while ((*p >= '0') && (*p <= '9')) {
-		ind += ind * 10 + (*p - '0');
+                int diff = max_num - 10 * ind - (*p - '0');
+                if (diff < 0) 
+		    throw Big_index("index is very big",  p - text.begin() + 1) ;
+		ind = ind * 10 + (*p - '0');
 		p++;
 	    }
             if (ind >= num_of_arg)
 		throw Bad_index("index is very big!", p - text.begin() + 1) ;
+            if (ind > max_ind)
+		max_ind = ind;
 	    if ((p == text.end()) || (*p != '}'))
 		throw Bad_bracket("expected } after index", p - text.begin() + 1) ;
 	    else
@@ -84,6 +108,8 @@ std::string format(const std::string & text, Args ... args) {
             result += arg.str();    
 	}
     }
+    if (max_ind < num_of_arg - 1)
+	throw Too_much_args("unused_args", p - text.begin() + 1) ;
     return result;    
 } 
 
--- 06/main.cpp
+++ 06/main.cpp
@@ -6,6 +6,18 @@ void ok_work_test() {
 }
 
 
+void base_class_exception_test() {
+     bool t = false;
+    try {
+        auto text = format("{1}+{1} = 0}", 2, "one");
+    }
+    catch(std::logic_error& b) {
+	t = true;
+    } 
+    assert(t);
+
+}
+
 void no_open_bracket_test() {
     bool t = false;
     try {
@@ -52,9 +64,16 @@ void single_brackets() {
 }
 
 
-void empty_string_test(){
-    auto text = format("", 2, "one","3","okay");
-    assert(text == "");
+void unused_args_test(){
+    bool p = false;
+    try {
+        auto text = format("{1} + {2}", 2, "one","3","okay");
+    }
+    catch(Too_much_args& b) {
+	b.info();
+	p = true;
+    }
+    assert(p);
 }
 
 void empty_index() {
@@ -93,15 +112,118 @@ void big_index() {
     assert(t);
 }
 
+void empty_string() {
+    auto text = format("");
+    assert(text == "");
+}
+
+void one_arg_test() {
+    auto text = format("{0}","hello");
+    assert(text == "hello");
+}
+
+void many_args_test() {
+    auto text = format("{1}{1}{1}{1}{1}{1}{1}","a","hello");
+    assert(text == "hellohellohellohellohellohellohello");  
+    auto text1 = format("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}{11}{12}","a","b","c","d","e","f","g","h","i","j","k","l","m");
+    assert(text1 == "abcdefghijklm");
+}
+
+void too_much_open_brackets() {
+    bool t = false;
+    try {
+        auto text = format("{1}+{1} = {{0}", 2, "one");
+    }
+    catch(Bad_index& b) {
+	b.info();
+	t = true;
+    }
+    assert(t);
+}
+
+
+void too_much_close_brackets() {
+    bool t = false;
+    try {
+        auto text = format("{1}+{1}} = {0}", 2, "one");
+    }
+    catch(Bad_bracket& b) {
+	b.info();
+	t = true;
+    }
+    assert(t);
+}
+
+
+void only_brackets() {
+    bool t = false;
+    try {
+        auto text = format("{}", 2, "one");
+    }
+    catch(Bad_index& b) {
+	b.info();
+	t = true;
+    }
+    assert(t);
+}
+
+
+void string_in_brackets() {
+    bool t = false;
+    try {
+        auto text = format("{hello}", 2, "one");
+    }
+    catch(Bad_index& b) {
+	b.info();
+	t = true;
+    }
+    assert(t);
+}
+
+void mix_of_num_and_string_in_brackets() {
+    bool t = false;
+    try {
+        auto text = format("{1e}", 2, "one");
+    }
+    catch(Bad_bracket& b) {
+	b.info();
+	t = true;
+    }
+    assert(t);
+}
+
+
+void big_int_in_brackets() {
+    bool t = false;
+    try {
+        auto text = format("{99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999}", 2);
+    }
+    catch(Big_index& b) {
+	b.info();
+	t = true;
+    }
+    assert(t);
+}
+
 int main() {
     ok_work_test();
+    empty_string();
+    one_arg_test();
+    many_args_test();  
     no_open_bracket_test();
+    base_class_exception_test();
     no_close_bracket_test();
     single_brackets();
-    empty_string_test();
+    unused_args_test();
     empty_index();
     wrong_index();
     big_index();
+    too_much_open_brackets();
+    too_much_close_brackets();   
+    only_brackets();
+    string_in_brackets();
+    mix_of_num_and_string_in_brackets(); 
+    big_int_in_brackets();
     return 0;
 }
 
